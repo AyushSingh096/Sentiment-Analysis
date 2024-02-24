a734a47bbd6e77d37fc89091e86352446534c289
AyushSingh096 <ayushsingh00112233@gmail.com>
2024-02-24 20:10:54 +0530

    project created and setup successfully

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sentiment-Analysis",
+    version = "0.0.1",
+    author = "ayushsingh",
+    author_email="ayushsingh00112233@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
